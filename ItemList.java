9b7b96b39cf7a466bd27a53a741010ea5bed686e
SHAIK ZAID <37359038+shaikzaid@users.noreply.github.com>
2023-07-04 13:43:39 +0530

    Update ItemList.java

--- ItemList.java
+++ ItemList.java
@@ -9,17 +9,11 @@ public class ItemList {
     private BasicTaxCalculator basicTaxCalculator;
     private ImportedTaxCalculator importedTaxCalculator;
 
-    public static double price1;
-
     public ItemList() {
         products = new ArrayList<>();
        basicTaxCalculator=new BasicTaxCalculator();
        importedTaxCalculator=new ImportedTaxCalculator();
     }
-
-      public List<Product> getProducts() {
-        return products;
-        }
 
     public double calculateTotalSales() {
         double totalSales = 0;
@@ -31,32 +25,60 @@ public class ItemList {
     public void addProduct(Product product, int quantity) {
         products.add(product);
         product.updatequantityPrice(quantity);
+
+
     }
 
     public double calculateSalesTaxes() {
         double salesTaxes = 0;
         double taxAmount = 0;
         for (Product product : products) {
-
-           //if product is imported then check wheather it belongs to exempted category
-            if (product.isImported()) {
-                if(exemptedProducts.isExempted(product.getName())){
-                taxAmount = importedTaxCalculator.calculateTax(product.getPrice());
-                }
-                else {
-                    taxAmount = importedTaxCalculator.calculateTax(product.getPrice())+ basicTaxCalculator.calculateTax(product.getPrice());
-                }
-            }
-             else {
-                if(!exemptedProducts.isExempted(product.getName())){
-                taxAmount = basicTaxCalculator.calculateTax(product.getPrice());}
-            }
+           taxAmount= calculatePWithTax(product);
              salesTaxes += taxAmount;
 
         }
         return salesTaxes;
     }
+
+    public double calculatePWithTax(Product product){
+        double taxAmount=0;
+
+        if (product.isImported()) {
+            if (exemptedProducts.isExempted(product.getName())) {
+                taxAmount = importedTaxCalculator.calculateTax(product.getPrice());
+            } else {
+                taxAmount = importedTaxCalculator.calculateTax(product.getPrice())
+                        + basicTaxCalculator.calculateTax(product.getPrice());
+            }
+        } else {
+            if (!exemptedProducts.isExempted(product.getName())) {
+                taxAmount = basicTaxCalculator.calculateTax(product.getPrice());
+            }
+        }
+        return taxAmount;
+    }
+
+    public List<taxPrice> calculatePricesWithTax() {
+        List<taxPrice> productOutputs = new ArrayList<>();
+
+        for (Product product : products) {
+            double taxAmount = 0;
+            taxAmount=calculatePWithTax(product);
+
+            double priceWithTax = product.getPrice() + taxAmount;
+
+            // Create a ProductOutput object
+            taxPrice productOutput = new taxPrice(product.getQuantity(), product.getName(),product.isImported(), priceWithTax);
+
+            // Add the ProductOutput object to the list
+            productOutputs.add(productOutput);
+            //product.updatePrice();
+        }
+
+        return productOutputs;
+    }
 }
+
 
 
 
